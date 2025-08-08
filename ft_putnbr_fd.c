c5d76f0b7b888556938a607f6158b00650559e97
aboodmomanni <aboodhanialmomni@gmail.com>
2025-08-09 00:44:21 +0300

    Add files via upload

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_putchar_fd(char c, int fd)
+{
+    write(fd, &c, 1);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+    long n = n;
+
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        n = -n;
+    }
+    if (n >= 10)
+        ft_putnbr_fd(n / 10, fd);
+    ft_putchar_fd((n % 10) + '0', fd);
+}
